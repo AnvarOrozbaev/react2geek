f4f16ea6b2a3f90b09f2f07593fbfcffd5c0bbb4
Anvar <voloklim@gmail.com>
2022-04-18 12:01:51 +0600

    lesson3

--- src/components/message-list/MessageList.test.js
+++ src/components/message-list/MessageList.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { MessageList } from './MessageList';
-import { render, screen,  fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
 
 describe('MessageList', () => {
   it('render component', () => {
-    const messages = [{text: 'hello', author: 'me'}]
-    render(<MessageList messages={messages}/>)
+    const messages = [{ text: 'hello', author: 'me' }];
+    render(<MessageList messages={messages} />);
   });
   it('render with snapshot', () => {
-    const messages = [{text: 'hello', author: 'me'}]
-    const { asFragment } = render(<MessageList messages={messages}/>)
+    const messages = [{ text: 'hello', author: 'me' }];
+    const { asFragment } = render(<MessageList messages={messages} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
